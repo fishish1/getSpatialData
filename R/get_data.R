e196151e03338de68e58f84c926cabd9a755ba4f
fishish1 <44877592+fishish1@users.noreply.github.com>
2020-08-07 10:51:28 -0600

    Update get_data.R

--- R/get_data.R
+++ R/get_data.R
@@ -100,7 +100,7 @@ get_data <- function(records, dir_out = NULL, md5_check = TRUE, force = FALSE, a
   
   # get URLs
   out("Assembling dataset URLs...")
-  records=as.data.frame(records)
+  #records=as.data.frame(records)
   records$dataset_url <- NA
   records[sub,]$dataset_url <- .get_ds_urls(records[sub,])
   out(records$dataset_url)
